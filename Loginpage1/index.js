3e7066160c4f2c4ffd3c203fda2af643672ac73a
Adnan Boulifa <adnanbolifa.9@gmail.com>
2022-10-15 21:28:16 +0200

    Loginpages test

--- Loginpage1/index.js
+++ Loginpage1/index.js
@@ -1,16 +1,21 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+const usrs = ["admin", "lol", "gg"];
+const psw = ["admin", "lol", "gg"];
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
+
     const username = loginForm.username.value;
     const password = loginForm.password.value;
-
-    if (username === "admin" && password === "admin") {
-        alert("You have successfully logged in.");
-        location.reload();
-    } else {
+    if (username === "admin" && password === "admin")
+    {
+        
+        location = "/";
+    } else
+    {
+        alert("Please enter a username and password");
         loginErrorMsg.style.opacity = 1;
     }
 })
